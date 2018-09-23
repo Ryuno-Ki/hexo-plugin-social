7b98ed4dbc3ce5d8a91c1d4c296b449b4bfa1121
André Jaenisch <andre.jaenisch@posteo.de>
2018-09-23 17:39:11 +0200

    Refactoring.

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
+'use strict';
 const hexo = require('hexo');
 
-hexo.entend.tag.register('twitter', function (args, content) {
-    const twitter = args[ 0 ];
-    return [
-      '<a',
-        `href="https://twitter.com/${ twitter }"`,
-        'class="twitter-follow-button"',
-        'data-button="blue"',
-        'data-text-color="#FFFFFF"',
-        'data-link-color="#FFFFFF"',
-        'data-show-screen-name="false"',
-        'data-show-count="true">',
-        `Follow @${ twitter }`,
-      '</a>'
-    ].join(' ');
-});
+const twitter = require('./social/twitter');
+const facebook = require('./social/facebook');
 
-hexo.entend.tag.register('facebook', function (args, content) {
-    const facebook = args[ 0 ];
-    return [
-      '<iframe',
-        'id="fb"',
-        `src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Ffacebook.com%2F${ facebook }&amp;send=false&amp;layout=button_count&amp;width=110&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font=segoe+ui&amp;height=20"`,
-        'scrolling="no"',
-        'frameborder="0"',
-        'allowTransparency="true">'
-      '</iframe>'
-    ].join(' ');
-});
+hexo.entend.tag.register('twitter', twitter);
+hexo.entend.tag.register('facebook', facebook);
---
+++ social/facebook.js
@@ -0,0 +1,15 @@
+'use strict';
+const facebook = function facebook (args) {
+    const facebook = args[ 0 ];
+    return [
+      '<iframe',
+        'id="fb"',
+        `src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Ffacebook.com%2F${ facebook }&amp;send=false&amp;layout=button_count&amp;width=110&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font=segoe+ui&amp;height=20"`,
+        'scrolling="no"',
+        'frameborder="0"',
+        'allowTransparency="true">'
+      '</iframe>'
+    ].join(' ');
+};
+
+module.exports = facebook;
